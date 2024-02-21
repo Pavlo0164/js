a528709ab5c616ea47864e9dc644ad78387961c2
Pavlo0164 <126184203+Pavlo0164@users.noreply.github.com>
2024-02-21 12:15:35 +0200

    Add files via upload

---
+++ home-work/lesson_1/js/app.js
@@ -0,0 +1,44 @@
+//сума трьох чисел
+const numberOne = 12;
+let a = parseInt(prompt("Number a ="));
+let b = parseInt(prompt("Number b ="));
+let sumNumbers = numberOne + a + b;
+
+document.write(`Hello.Sum of numbers :   ${sumNumbers}`);
+
+//сума,добуток,частка двох чисел
+alert(
+  `Сума чисел = ${a + b}\n 
+   Добуток чисел = ${a * b}\n 
+   Частка чисел = ${a / b} \n`
+);
+
+//Кількість ваших років
+const yearNow = 2024;
+let yearBirthday = parseInt(prompt(`Введіть рік свого народження`));
+let yearsOld = yearNow - yearBirthday;
+
+document.write(`You are ${yearsOld} years old`);
+alert(`You are ${yearsOld} years old`);
+
+//Задача 3.
+let priceProduct = parseInt(prompt(`Введіть вартість товару у $`));
+let quantityProduct = parseInt(prompt(`Введіть кількість товару`));
+const percentTax = 5;
+
+let generalPriceProduct = priceProduct + quantityProduct;
+let priceTax = (generalPriceProduct / 100) * percentTax;
+
+alert(
+  `Загальна вартість товару ${generalPriceProduct} $ \n
+    Загальна вартість ПДВ від суми товару ${priceTax} $`
+);
+
+//задача 4.
+let lengthInCentimetr = parseInt(prompt(`Введіть довжину у сантиметрах`));
+let lenthInMetrs = lengthInCentimetr / 100;
+let lenthInKilometrs = lenthInMetrs / 1000;
+
+alert(`Довжина у метрах становить ${lenthInMetrs} м \n
+        Довжина у кілометрах становить ${lenthInKilometrs} км
+ `);
