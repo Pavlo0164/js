5bb1ea018e14d17d7bbb61e2c8b9e74a888c3430
Pavlo Podolian <pavlikpodolyan7525@gmail.com>
2024-02-24 19:46:49 +0200

    eee

--- lesson_2/tasks/task-4/js/app.js
+++ lesson_2/tasks/task-4/js/app.js
@@ -3,9 +3,11 @@ let minMonth = 1;
 let maxMonth = 12;
 let minDay = 0;
 let maxDay = 6;
-let randomNumberMonth =
-  Math.floor(Math.random() * (maxMonth - minMonth + 1)) + minMonth;
-let randomNumberDay =
-  Math.floor(Math.random() * (maxDay - minDay + 1)) + minDay;
+let randomNumberMonth = Math.floor(
+  Math.random() * (maxMonth - minMonth + 1) + minMonth
+);
+let randomNumberDay = Math.floor(
+  Math.random() * (maxMonth - minMonth + 1) + minMonth
+);
 let sumRandomNumber = randomNumberMonth + randomNumberDay;
 document.write(`Сума випадкових чисел : ${sumRandomNumber}`);
