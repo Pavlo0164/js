75db2270a59592e181469d009324d6070b369fbf
Pavlo Podolian <pavlikpodolyan7525@gmail.com>
2024-03-15 11:16:36 +0100

    lesson7

--- lesson_7/tasks/task-10/js/app.js
+++ lesson_7/tasks/task-10/js/app.js
@@ -3,5 +3,20 @@ alert(`
  Розробити функцію, яка дозволить підрахувати кількість від’ємних показів температури.
 `);
 function showTemp() {
-    
+	let count = 0;
+	for (let i = 0; i < arguments.length; i++) {
+		if (arguments[i] < 0) count += 1;
+	}
+	return count;
 }
+
+document.write(
+	`Кількість від'ємних значень температури становить ${showTemp(
+		32,
+		23,
+		23,
+		14,
+		23,
+		34
+	)}`
+);
