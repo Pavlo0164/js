200d27db3de2566f971c613b5e38de7586140415
Pavlo Podolian <pavlikpodolyan7525@gmail.com>
2024-03-29 12:56:44 +0100

    lesson11Comlete

--- lesson_11/tasks/task-3/js/app.js
+++ lesson_11/tasks/task-3/js/app.js
@@ -1,31 +1,53 @@
-document.write(
-	`Задача 1. Знайти суми елементів у вказаній області </br>
-	Пункт1:номери рядків від 0 до половини, стовпці від 0 до половини 
-`
-);
+//Задача 3. Морський бій. Випадковим чином на двовимірному полі розміром 6*6 розташовується 5 кораблів.
+//Користувач стріляє вказуючи координати.
+//Гра продовжується поки не потоплено усі кораблі або у користувача не закінчаться снаряди.
 
-function generateArray(row, column, max, min) {
-	let arr = [];
-	for (let i = 0; i < row; i++) {
+function generateField() {
+	let field = [];
+	for (let i = 0; i < 6; i++) {
 		let subArr = [];
-		for (let j = 0; j < column; j++) {
-			let num = min + Math.floor(Math.random() * (max - min) + 1);
-			subArr.push(num);
+		for (let p = 0; p < 6; p++) {
+			subArr.push(0);
 		}
-		arr.push(subArr);
+		field.push(subArr);
 	}
-	return arr;
-}
-let newArr = generateArray(4, 4, 3, 10);
-
-function getSum(arr) {
-	let sum = 0;
-	for (let i = 0; i < arr.length / 2; i++) {
-		for (let j = 0; j < arr[i].length / 2; j++) {
-			sum += arr[i][j];
+	let count = 5;
+	while (count) {
+		let x = Math.floor(Math.random() * 6);
+		let y = Math.floor(Math.random() * 6);
+		if (field[y][x] === 0) {
+			field[y][x] = 1;
+			count--;
 		}
 	}
-	return sum;
+	console.log(field);
+	return field;
 }
-
-document.write(`:${getSum(newArr)}`);
+function gameSeaFight(funcGenerateField) {
+	let field = funcGenerateField()
+	let shots = 10;
+	let count = 0;
+	alert("Ти маєш десять набоїв щоб потопити корабель");
+	do {
+		let y = parseInt(prompt("Введіть вертикальну координату пострілу від 0 до 5"));
+		let x = parseInt(prompt("Введіть горизонтальну координату пострілу від 0 до 5"));
+		if (field[y][x] === 1) {
+			field[y][x] = "X";
+			count++;
+			alert("Вітаю, ви знищили один корабель");
+		} else {
+			alert('Нажаль ви не попали');
+		}
+		shots--;
+	} while (shots);
+	let resultGame = field.flat().find((item) => item === 1);
+	if (resultGame) alert("Нажаль ви не виграли");
+	else alert("Вітаю, ви виграли");
+	document.write(`1 - це корабель </br> X - це знищенний корабель </br>
+	`)
+	field.forEach(element => {
+		document.write(`${element}</br>`)
+	});
+}
+let startGame = confirm("Бажаєте розпочати гру?");
+if (startGame) gameSeaFight(generateField);
