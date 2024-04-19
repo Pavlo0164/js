f869698af7aaf40d6f410149fcb1303832cf5a25
Pavlo Podolian <pavlikpodolyan7525@gmail.com>
2024-04-19 16:26:16 +0200

    changeDay

--- lesson_16/tasks/task-1/js/app.js
+++ lesson_16/tasks/task-1/js/app.js
@@ -14,80 +14,59 @@ class TDate {
 	get Day() {
 		return this.#day;
 	}
-	set Day(day) {
-		if (day < 1 || day > 31) throw new Error("Щось пішло не так");
-		this.#day = day;
-	}
 	get Month() {
 		return this.#month;
-	}
-	set Month(month) {
-		if (month < 1 || month > 12) throw new Error("Щось пішло не так");
-		this.#month = month;
 	}
 	get Year() {
 		return this.#year;
 	}
+	set Day(day) {
+		if (isNaN(day) || day < 1) throw new Error(`Не коректний день`);
+		else {
+			if (this.Day) {
+				this.#day = (this.Day + day) % 31;
+				this.#month += Math.floor((this.Day + day) / 31);
+			} else {
+				this.#day = day % 31;
+				this.#month = Math.floor(day / 31);
+			}
+		}
+	}
+
+	set Month(month) {
+		if (isNaN(month) || month < 1) throw new Error(`Не коректний місяць`);
+		if (this.Month) {
+			this.#month = (this.#month + month) % 12;
+			this.#year += Math.floor((this.Month + month) / 12);
+		} else {
+			this.#month = month % 12;
+			this.#year = Math.floor(month / 12);
+		}
+	}
+
 	set Year(year) {
-		if (year < 1 || year > 3000) throw new Error("Щось пішло не так");
-		this.#year = year;
+		if (isNaN(year) || year < 1 || year > 3000) throw new Error(`Не коректний рік`);
+		if (this.Year) {
+			this.#year = year + this.Year;
+		} else this.#year = year;
 	}
 	toString() {
 		return `${this.Day}.${this.Month}.${this.Year}`;
 	}
-	renderDate() {
-		if (this.Day > 31) {
-			this.#month += Math.floor(this.Day / 31);
-			this.#day = this.Day % 31;
-		}
-		if (this.Month > 12) {
-			this.#year += Math.floor(this.Month / 12);
-			this.#month = this.Month % 12;
-		}
+	changeDay(day) {
+		this.Day = day;
 	}
-	addDay(day) {
-		this.#day += day;
-		this.renderDate();
+	changeMonth(month) {
+		this.Month = month;
 	}
-	addMonth(month) {
-		this.#month += month;
-		this.renderDate();
-	}
-	addYear(year) {
-		if (this.Year + year > 3000) throw new Error("Не коректна кількість років");
-		this.#year += year;
-	}
-	renderDeleteDate() {
-		if (this.Day < 1) {
-			let numMonth = Math.ceil(Math.abs(this.Day) / 31);
-			this.#month -= numMonth;
-			this.#day = numMonth * 31 - Math.abs(this.Day);
-		}
-		if (this.Month < 1) {
-			let numYear = Math.ceil(Math.abs(this.Month) / 12);
-			this.#year -= numYear;
-			this.#month = numYear * 12 - Math.abs(this.Month);
-		}
-	}
-	deleteDay(day) {
-		this.#day -= day;
-		this.renderDeleteDate();
-	}
-	deleteMonth(month) {
-		this.#month -= month;
-		this.renderDeleteDate();
-	}
-	deleteYear(year) {
-		if (this.Year - year < 1) throw new Error("Не коректний рік");
-		this.#year -= year;
+	changeYear(year) {
+		this.Year = year;
 	}
 }
-let date = new TDate(12, 1, 2000);
+
+let date = new TDate(12,4,2000);
 console.log(date);
-date.addDay(40);
+date.changeDay(34)
 console.log(date);
-date.deleteDay(23);
+date.changeMonth(13)
 console.log(date);
-date.deleteMonth(2);
-console.log(date);
-document.write(date)
